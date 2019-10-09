cc3d1b73db849a3cbe380e42974e6a6d01283840
guojunyi84 <1021545981@qq.com>
2019-10-09 20:52:11 +0800

    'init1'

---
+++ nannvdapei/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home">
+    这里是主城初始化项目
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name:'home',
+  components:{
+	  HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
